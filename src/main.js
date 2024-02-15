69d005c179fb3f03d4efec023a247b40a7136aa8
Jackson McLane <jmclane@ucsc.edu>
2024-02-15 02:01:58 -0800

    updates main.js comments

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 /** Samurai Run by Jackson McLane
  *  Inspired by Samurai Blitz by Studio Thunderhorse
+ *  Hours Spent on game: 22 hours(?) roughly
+ *  Creative Tilt:
+ *  As far as technically interesting, although hacky im pretty proud of how I got my "sword" hitbox working,
+ *  the hitbox follows the player character and disappears when the player is not attacking, I would like to refactor
+ *  the system to more elegantly use Phaser containers but for now I am proud that it is functional
+ * 
+ *  In terms of art style I am proud of the swing animation I made! I used references from the game I was inspired by
+ *  to try and convey a sense of movement. I also used ChipTone to create sound effects that I quite like. 
+ * 
+ *  Another slight detail is that while the background does not actually have paralax, 
+ *  the drawing I made fakes it by having varying sizes of trees :P
  */
 
 let config  = {
